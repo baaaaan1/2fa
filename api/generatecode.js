ed5493212151996185a48f48829813b15c105da8
Ilham Ramadan <ilhamrs019.all@gmail.com>
2025-06-09 09:55:03 +0700

    first commit

---
+++ api/generatecode.js
@@ -0,0 +1,14 @@
+const { authenticator } = require('otplib');
+
+module.exports = (req, res) => {
+  const { secret } = req.query;
+  if (!secret) {
+    return res.status(400).json({ error: 'Missing secret' });
+  }
+  try {
+    const code = authenticator.generate(secret);
+    res.status(200).json({ code });
+  } catch (e) {
+    res.status(400).json({ error: 'Invalid secret' });
+  }
+};
